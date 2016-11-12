f632c2718ca2a2f265e54ec71b9e0514c3d8dc3c
jlanej <jlanej@gmail.com>
2016-11-12 09:41:13 -0600

    save before break

--- markerMaker/markerMaker.go
+++ markerMaker/markerMaker.go
@@ -13,19 +13,6 @@ import (
 	"appengine/datastore"
 )
 
-// current information returned for query
-// queries can be set up to handle pos<X&&chr==2, etc. This would allow getting all markers for an interval across any/all arrays
-// could also do RS ID searches, etc
-type Marker struct {
-	MarkerName string   `json:"markerName"`
-	RSId       string   `json:"rsID"`
-	Chromosome string   `json:"chromosome"`
-	Position   int      `json:"position"`
-	A_Allele   string   `json:"a_allele"`
-	B_Allele   string   `json:"b_allele"`
-	Arrays     []string `json:"arrays"` //which arrays this marker is present on
-}
-
 //template html for displaying results
 const (
 	PrintTemplate = `
